5280698e4197f958ead99db1087a88c4be15f3f1
Andrés Velasco <velasco.andrs@gmail.com>
2018-10-10 08:53:07 -0500

    condicional

---
+++ 01-Python/02_condicional.py
@@ -0,0 +1,30 @@
+# Truthy
+# Falsy
+
+if True:
+    print("Es verdad")
+else:
+    print("No es verdad")
+
+
+if False:
+    print("Es verdad")
+else:
+    print("No es verdad")
+
+nombre = 'Andres'
+
+if -1 or not 0:  # Truthy
+    print("Si")
+else:
+    print("No")
+
+
+diccionario = []
+if diccionario:  # Falsy
+    print("Si")
+else:
+    print("No")
+
+
+
