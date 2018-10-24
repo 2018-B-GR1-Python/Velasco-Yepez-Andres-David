1312e14666aa3929256ebc8c16699405dd774054
andres velasco <velasco.andrs@gmail.com>
2018-10-24 09:45:46 -0500

    importaciones

--- 01-Python/09_classes.py
+++ 01-Python/09_classes.py
@@ -1,69 +1,73 @@
+class Escuela:
+    # valor_categoria = 4
+    __ciudad = 'Quito'  # atributo privado
+    pais = 'Ecuador'  # atributo publico
+
+    def __init__(self, nombre, valor_categoria=4):
+        print(self)
+        print('Hola constructor')
+        self.nombre = nombre
+        self.valor_categoria = valor_categoria
+
+    def saludar(self):
+        print(f'Hola desde {self.nombre} localizada en'
+              f' {self.__ciudad} - {self.pais}')
+
+    def categoria(self):
+        return self.__calcular_categoria()
+
+    def __calcular_categoria(self):  # metodo privado
+        return self.valor_categoria * 3
+
+    def __str__(self):
+        return 'Escuela'
+
+
+twa = Escuela('Theodoro Winword Anderson')
+twa.valor_categoria = 2
+twa.saludar()
+print(twa.categoria())
+
+
 class Auto:
-    __ensamblado = 'Quito'
+    _ensamblado = 'Quito'
     numero_asientos = 5
 
-
-    def __init__(self,nombre, color):
+    def __init__(self, nombre, color):
         self.nombre = nombre
         self.color = color
 
-    def cambiar_ensamblado(self,ensamblado):
-        self.__ensamblado = ensamblado
+    def __init__(self, nombre, color, color_techo=''):
+        self.nombre = nombre
+        self.color = color
+        self.color_techo = color_techo
+
+    def cambiar_ensamblado(self, ensamblado):
+        self._ensamblado = ensamblado
 
     def __maximo_numero_pasajeros(self):
         return self.numero_asientos + 3
 
     def __str__(self):
-        print(self.nombre)
-        print(self.color)
-        print(self.__ensamblado)
-        print(self.numero_asientos)
-        print(self.__maximo_numero_pasajeros())
+        return (f"{self.nombre}\n"
+                f"{self.color}\n"
+                f"{self.color_techo}\n"
+                f"{self.numero_asientos}\n"
+                f"{self._ensamblado}\n"
+                f"{self.__maximo_numero_pasajeros()}\n")
+
+
+bmw = Auto('Blanco', 'Version 1')
+print(bmw)
 
 
 class Hyundai(Auto):
-    def __init__(self,color,nombre):
-        super().__init__(color=color,nombre=nombre)
-        print('construcor')
-        print(self.__ensamblado)
+
+    def __init__(self, color, nombre):
+        super().__init__(color=color, nombre=nombre)
+        print('constructor')
+        print(self._ensamblado)
 
 
-class Escuela:
-
-    __ciudad = 'Quito'  # atributo privado
-    pais = 'Ecuador'    # atributo publico
-
-
-    def __init__(self,nombre,valor_categoria):
-        print(self)
-        print("Hola constructor")
-        self.nombre = nombre
-        self.valor_categoria = valor_categoria
-
-    def saludar(self):
-        print(f'Hola desde {self.nombre}'
-              f' localizada en {self.__ciudad}')
-
-    def categoria(self):
-        return self.__calular_categoria()
-
-    #Metodo privado
-    def __calular_categoria(self):
-        return self.valor_categoria*3
-
-    # Metodo toString
-    def __str__(self):
-        return 'Escuela'
-
-
-
-
-twa = Escuela(nombre='FESVIP',valor_categoria=4)
-twa.saludar()
-
-
-bm = Auto(color='Blanco',nombre='V 3')
-print(bm)
-
-bm.cambiar_ensamblado('Guayaquil')
-print(bm)
+mi_carro = Hyundai('Negro', 'Santa fe')
+print(mi_carro)
